3f7533ad5c6bf4c9d3c4642861fe3898e5297cbd
kairee <364298+krwu@users.noreply.github.com>
2018-06-01 18:23:23 +0800

    fix(error): catch unhandledRejection and log it

--- app/handler/crawler.js
+++ app/handler/crawler.js
@@ -60,6 +60,11 @@ function checkDevice(defaults) {
   return { isMobile, device };
 }
 
+process.on('unhandledRejection', (reason, p) => {
+  console.log('未处理的 rejection：', p, '原因：', reason);
+  throw new Error(reason);
+});
+
 async function crawler(startUrl, options = {}) {
   let defaults = {
     loadImages: false,
