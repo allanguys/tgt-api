0d84e2a47cb01f41b69503ecb63ec06cf91ccde2
kairee <364298+krwu@users.noreply.github.com>
2018-06-01 17:16:54 +0800

    add two servers for deployment.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,38 +1,38 @@
 module.exports = {
-  apps : [{
-      name      : 'API',
-      script    : './server.js',
-      env: {
-        COMMON_VARIABLE: 'true'
-      },
-      env_production : {
-        NODE_ENV: 'production'
-      }
+  apps: [{
+    name: 'API',
+    script: './server.js',
+    env: {
+      COMMON_VARIABLE: 'true',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
   }],
 
   /**
    * Deployment section
    * http://pm2.keymetrics.io/docs/usage/deployment/
    */
-  deploy : {
-    production : {
-      user : 'node',
-      host : 'jdkvm',
-      ref  : 'origin/master',
-      repo : 'https://github.com/krwu/tgt-api.git',
-      path : '/home/node/tgt-api',
-      'post-deploy' : 'yarn install --prod && pm2 startOrRestart ecosystem.config.js --env production && pm2 save'
+  deploy: {
+    production: {
+      user: 'node',
+      host: ['jdkvm', 'qc3kvm', 'ks3c'],
+      ref: 'origin/master',
+      repo: 'https://github.com/krwu/tgt-api.git',
+      path: '/home/node/tgt-api',
+      'post-deploy': 'yarn install --prod && ln -sfv ../shared/config.json config.json && pm2 startOrRestart ecosystem.config.js --env production && pm2 save',
     },
-//    dev : {
-//      user : 'node',
-//      host : '212.83.163.1',
-//      ref  : 'origin/master',
-//      repo : 'git@github.com:repo.git',
-//      path : '/var/www/development',
-//      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
-//      env  : {
-//        NODE_ENV: 'dev'
-//      }
-//    }
-  }
+    //    dev : {
+    //      user : 'node',
+    //      host : '212.83.163.1',
+    //      ref  : 'origin/master',
+    //      repo : 'git@github.com:repo.git',
+    //      path : '/var/www/development',
+    //      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env dev',
+    //      env  : {
+    //        NODE_ENV: 'dev'
+    //      }
+    //    }
+  },
 };
