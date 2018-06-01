57495af19ddaddf5b8affdffa6cabc02215d9c1f
kairee <364298+krwu@users.noreply.github.com>
2018-06-01 18:45:25 +0800

    增加超时时长，捕获puppeteer抛出的异常。

--- app/handler/crawler.js
+++ app/handler/crawler.js
@@ -98,179 +98,182 @@ async function crawler(startUrl, options = {}) {
     launchOptions.executablePath = chrome;
   }
 
-  // try {
-  const browser = await puppeteer.launch(launchOptions);
-  result.browser = await browser.version();
+  try {
+    const browser = await puppeteer.launch(launchOptions);
+    result.browser = await browser.version();
 
-  const page = await browser.newPage();
-  await page.setRequestInterception(true);
+    const page = await browser.newPage();
+    await page.setRequestInterception(true);
 
-  if (defaults.media && ['screen', 'print'].includes(defaults.media)) {
-    await page.emulateMedia(defaults.media);
-  }
-
-  let { isMobile, device } = checkDevice(defaults);
-
-  await page.emulate(device);
-
-  // log console messages
-  if (defaults.logConsole) {
-    page.on('console', (consoleMessage) => {
-      result.addConsole(consoleMessage.type(), consoleMessage.text());
-    });
-  }
-
-  page.on('dialog', async (dialog) => {
-    await dialog.dismiss();
-  });
-
-  // log dom ready time
-  page.on('domcontentloaded', () => {
-    result.setTimer('domContentLoaded', Date.now());
-  });
-
-  page.on('error', async (err) => {
-    hasError = true;
-    errMsg = `Page error. ${err.message}`;
-    console.log(errMsg);
-    await browser.close();
-  });
-
-  page.on('pageerror', (err) => {
-    result.addError(err.message);
-  });
-
-  // check and log redirect
-  page.on('framenavigated', async (frame) => {
-    if (frame === page.mainFrame()) {
-      const toUrl = frame.url();
-      if (toUrl !== url && toUrl !== 'about:blank') {
-        if (!defaults.followRedirect) {
-          if (!retried) {
-            // change device and retry
-            result.clear();
-            result.redirects = [url];
-            device = isMobile ? pcDevice : devices['iPhone X'];
-            console.warn('retied with', device.userAgent);
-            isMobile = !isMobile;
-            retried = true;
-            return Promise.all([
-              page.emulate(device),
-              page.goto(url, {
-                timeout: 10000,
-                waitUntil: 'networkidle0',
-              }),
-            ]);
-          }
-          hasError = true;
-          errMsg = `Stop follow redirec from "${url}" to "${toUrl}".`;
-        } else {
-          result.clear();
-          result.addRedirect(toUrl);
-          url = toUrl;
-        }
-      } else {
-        result.setTimer('navigated', Date.now());
-      }
+    if (defaults.media && ['screen', 'print'].includes(defaults.media)) {
+      await page.emulateMedia(defaults.media);
     }
-    return Promise.resolve();
-  });
 
-  page.on('response', (res) => {
-    if (res.url() === url && res.headers()) {
-      // hasError = true;
-      // errMsg = `Main Request Error (${res.status()}).`;
-      result.setHeaders(res.headers());
-      const contentType = res.headers()['content-type'] || '';
-      const match = contentType.match(/charset=([a-zA-Z0-9_-]+)/i);
-      result.charset = (match && match.length >= 2) ? match[1] : '';
-    }
-  });
+    let { isMobile, device } = checkDevice(defaults);
 
-  page.on('load', () => {
-    result.setTimer('load', Date.now());
-  });
+    await page.emulate(device);
 
-  page.on('request', (req) => {
-    // Stop login redirect
-    if (req.url().indexOf('//login.game.qq.com/comm-cgi-bin/login') > 0 ||
-        req.url().indexOf('/comm-htdocs/milo_mobile/login.html') > 0) {
-      req.respond({
-        status: '200',
-        contentType: 'text/plain',
-        body: '',
+    // log console messages
+    if (defaults.logConsole) {
+      page.on('console', (consoleMessage) => {
+        result.addConsole(consoleMessage.type(), consoleMessage.text());
       });
-    } else {
-      const type = req.resourceType();
-      if (req.url() === url) {
-        result.setTimer('request', Date.now());
-      }
-      if (defaults.logRequests) {
-        result.setRequest(req.url(), '100');
-      }
-      if (type === 'image' && !defaults.loadImages) {
-        if (defaults.logRequests) {
-          result.setRequest(req.url(), 'Aborted');
-        }
-        req.abort('aborted');
-      } else if (type === 'media' && !defaults.loadMedias) {
-        if (defaults.logRequests) {
-          result.setRequest(req.url(), 'Aborted');
-        }
-        req.abort('aborted');
-      } else {
-        req.continue();
-      }
     }
-  });
 
-  page.on('requestfinished', async (req) => {
-    try {
-      if (req.url() === url) {
-        result.setTimer('requestfinished', Date.now());
-        result.setHeaders(req.response().headers());
-        const contentType = req.response().headers()['content-type'] || '';
+    page.on('dialog', async (dialog) => {
+      await dialog.dismiss();
+    });
+
+    // log dom ready time
+    page.on('domcontentloaded', () => {
+      result.setTimer('domContentLoaded', Date.now());
+    });
+
+    page.on('error', async (err) => {
+      hasError = true;
+      errMsg = `Page error. ${err.message}`;
+      console.log(errMsg);
+      await browser.close();
+    });
+
+    page.on('pageerror', (err) => {
+      result.addError(err.message);
+    });
+
+    // check and log redirect
+    page.on('framenavigated', async (frame) => {
+      if (frame === page.mainFrame()) {
+        const toUrl = frame.url();
+        if (toUrl !== url && toUrl !== 'about:blank') {
+          if (!defaults.followRedirect) {
+            if (!retried) {
+              // change device and retry
+              result.clear();
+              result.redirects = [url];
+              device = isMobile ? pcDevice : devices['iPhone X'];
+              console.warn('retied with', device.userAgent);
+              isMobile = !isMobile;
+              retried = true;
+              return Promise.all([
+                page.emulate(device),
+                page.goto(url, {
+                  timeout: 15000,
+                  waitUntil: 'networkidle2',
+                }),
+              ]);
+            }
+            hasError = true;
+            errMsg = `Stop follow redirec from "${url}" to "${toUrl}".`;
+          } else {
+            result.clear();
+            result.addRedirect(toUrl);
+            url = toUrl;
+          }
+        } else {
+          result.setTimer('navigated', Date.now());
+        }
+      }
+      return Promise.resolve();
+    });
+
+    page.on('response', (res) => {
+      if (res.url() === url && res.headers()) {
+        // hasError = true;
+        // errMsg = `Main Request Error (${res.status()}).`;
+        result.setHeaders(res.headers());
+        const contentType = res.headers()['content-type'] || '';
         const match = contentType.match(/charset=([a-zA-Z0-9_-]+)/i);
         result.charset = (match && match.length >= 2) ? match[1] : '';
       }
-      if (defaults.logRequests) {
+    });
+
+    page.on('load', () => {
+      result.setTimer('load', Date.now());
+    });
+
+    page.on('request', (req) => {
+      // Stop login redirect
+      if (req.url().indexOf('//login.game.qq.com/comm-cgi-bin/login') > 0 ||
+        req.url().indexOf('/comm-htdocs/milo_mobile/login.html') > 0) {
+        req.respond({
+          status: '200',
+          contentType: 'text/plain',
+          body: '',
+        });
+      } else {
+        const type = req.resourceType();
+        if (req.url() === url) {
+          result.setTimer('request', Date.now());
+        }
+        if (defaults.logRequests) {
+          result.setRequest(req.url(), '100');
+        }
+        if (type === 'image' && !defaults.loadImages) {
+          if (defaults.logRequests) {
+            result.setRequest(req.url(), 'Aborted');
+          }
+          req.abort('aborted');
+        } else if (type === 'media' && !defaults.loadMedias) {
+          if (defaults.logRequests) {
+            result.setRequest(req.url(), 'Aborted');
+          }
+          req.abort('aborted');
+        } else {
+          req.continue();
+        }
+      }
+    });
+
+    page.on('requestfinished', async (req) => {
+      try {
+        if (req.url() === url) {
+          result.setTimer('requestfinished', Date.now());
+          result.setHeaders(req.response().headers());
+          const contentType = req.response().headers()['content-type'] || '';
+          const match = contentType.match(/charset=([a-zA-Z0-9_-]+)/i);
+          result.charset = (match && match.length >= 2) ? match[1] : '';
+        }
+        if (defaults.logRequests) {
+          result.setRequest(req.url(), req.response().status());
+        }
+      } catch (err) {
+        throw (err);
+      }
+    });
+
+    page.on('requestfailed', async (req) => {
+      if (req.url() === url && req.failure().errorText !== 'net::ERR_ABORTED') {
+        hasError = true;
+        errMsg = `Request failed. [ ${req.url()}: ${req.failure().errorText} ]`;
+        result.setTimer('requestfailed', Date.now());
+      }
+      if (req.response()) {
         result.setRequest(req.url(), req.response().status());
       }
-    } catch (err) {
-      throw (err);
-    }
-  });
+    });
 
-  page.on('requestfailed', async (req) => {
-    if (req.url() === url && req.failure().errorText !== 'net::ERR_ABORTED') {
-      hasError = true;
-      errMsg = `Request failed. [ ${req.url()}: ${req.failure().errorText} ]`;
-      result.setTimer('requestfailed', Date.now());
+    result.setTimer('start', Date.now());
+    await page.goto(url, {
+      timeout: 15000,
+      waitUntil: 'networkidle2',
+    });
+    const html = await page.content();
+    if (defaults.logHtml) {
+      result.html = html;
     }
-    if (req.response()) {
-      result.setRequest(req.url(), req.response().status());
+    if (result.charset === '') {
+      const match = html.match(/<meta[^>]+charset=["']?([a-zA-Z0-9_-]+)['";]*>/i);
+      result.charset = (match && match.length >= 2) ? match[1] : '';
     }
-  });
 
-  result.setTimer('start', Date.now());
-  await page.goto(url, {
-    timeout: 10000,
-    waitUntil: 'networkidle0',
-  });
-  const html = await page.content();
-  if (defaults.logHtml) {
-    result.html = html;
+    result.url = url;
+    result.userAgent = await page.evaluate(() => Promise.resolve(window.navigator.userAgent));
+    await browser.close();
+
+    return hasError ? Promise.reject(new Error(errMsg)) : Promise.resolve(result);
+  } catch (err) {
+    return Promise.reject(err);
   }
-  if (result.charset === '') {
-    const match = html.match(/<meta[^>]+charset=["']?([a-zA-Z0-9_-]+)['";]*>/i);
-    result.charset = (match && match.length >= 2) ? match[1] : '';
-  }
-
-  result.url = url;
-  result.userAgent = await page.evaluate(() => Promise.resolve(window.navigator.userAgent));
-  await browser.close();
-
-  return hasError ? Promise.reject(new Error(errMsg)) : Promise.resolve(result);
 }
 
 module.exports = crawler;
